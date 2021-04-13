6385d91bb1997b6e72938b29888fbbed4cd991bb
K Bhavani Venkata Karthik <80912980+KBVKarthik@users.noreply.github.com>
2021-04-13 13:08:05 +0530

    Add files via upload

---
+++ Password_generator.py
@@ -0,0 +1,15 @@
+import random
+
+chars='abcdefghijklmnopqrstuvwxyz1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*'
+
+number=input('Number of passwords? - ')
+number=int(number)
+
+length=input('Password length? - ')
+length=int(length)
+
+for p in range(number):
+    password=' '
+    for c in range(length):
+        password+=random.choice(chars)
+    print(password)
